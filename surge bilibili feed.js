07986148b347c97db727c50588df8f2d32b7a5ee
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-05-13 22:27:54 +0800

    Update surge bilibili feed.js

--- surge bilibili feed.js
+++ surge bilibili feed.js
@@ -5,7 +5,9 @@ obj1=obj['data']['items'];
 
 obj1.forEach(function (element, index, array) {
    
-    if(element.hasOwnProperty('ad_info')||element.hasOwnProperty('banner_item')||element['card_type']=="three_item_h_v2"){      
+    if(element.hasOwnProperty('ad_info')||element.hasOwnProperty('banner_item')||element['card_type']=="three_item_h_v2"){ 
+       //for testing log function
+       console.log(index)
        obj1.splice(index,1)  
     }
 
