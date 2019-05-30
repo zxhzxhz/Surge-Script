cfe94b83593f128daaf9b3e2060116fad912199f
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-05-30 20:08:51 +0800

    Update surge bilibili view relate.js

--- surge bilibili view relate.js
+++ surge bilibili view relate.js
@@ -1,16 +1,8 @@
 var obj = JSON.parse(body); 
-
-obj1=obj['data']['relates'];
-
-obj1.forEach(function (element, index, array) {
-   
-    if(element.hasOwnProperty('is_ad')){      
-       obj1.splice(index,1)  
+obj['data']['relates'].forEach((element, index)=> {
+   if(element.hasOwnProperty('is_ad')){      
+      obj['data']['relates'].splice(index,1)  
     }
-
-   });
-obj['data']['relates']=obj1
-
+});
 delete obj['data']['cms']
-
 JSON.stringify(obj);
