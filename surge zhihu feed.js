fb304bbe3ffbb4cc8c9eec17abdfe9c91f122986
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-05-30 20:05:46 +0800

    Update surge zhihu feed.js

--- surge zhihu feed.js
+++ surge zhihu feed.js
@@ -1,13 +1,7 @@
 var obj = JSON.parse(body); 
-obj1=obj['data'];
-
-obj1.forEach(function (element, index, array) {
+obj['data'].forEach((element, index)=>{
      if(element.hasOwnProperty('ad')){      
        obj1.splice(index,1)  
     }
-
-   });
-
-obj['data']=obj1
-
+});
 JSON.stringify(obj); 
