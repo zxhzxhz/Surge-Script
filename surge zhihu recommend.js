986d931aec7082caf5865d1b1094c8e070e2bc26
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-05-30 20:03:01 +0800

    Update surge zhihu recommend.js

--- surge zhihu recommend.js
+++ surge zhihu recommend.js
@@ -1,15 +1,7 @@
-
 var obj = JSON.parse(body); 
-
-obj1=obj['data'];
-
-obj1.forEach(function (element, index, array) {
+obj['data'].forEach((element, index)=> {
     if(element['card_type']=="slot_event_card"||element.hasOwnProperty('ad')){      
-       obj1.splice(index,1)  
+       obj['data'].splice(index,1)  
     }
-
-   });
-
-obj['data']=obj1
-
+});
 JSON.stringify(obj); 
