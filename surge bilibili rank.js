74203c55fdeb7fc6ef0537b5046bb286356f37e1
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-06-21 21:48:23 +0800

    Update surge bilibili rank.js

--- surge bilibili rank.js
+++ surge bilibili rank.js
@@ -1,9 +1,8 @@
-var obj = JSON.parse(body); 
-obj['data'].forEach((element, index)=> {
+let body = $response.body
+body['data'].forEach((element, index)=> {
     //block title||up
-   if(["华为","小米"].includes(element['title'])||["共青团中央","广东共青团","浙江共青团","徐大sao","翔翔大作战","徐大虾咯","科技美学","敬汉卿","NathanRich火锅大王","千户长生"].includes(element['name'])){ 
-         obj['data'].splice(index,1)  
+   if(["共青团中央","广东共青团","浙江共青团","徐大sao","翔翔大作战","徐大虾咯","科技美学","敬汉卿","NathanRich火锅大王","千户长生"].includes(element['name'])){ 
+         body['data'].splice(index,1)  
     }
-
-});
-JSON.stringify(obj);
+})
+$done({body})
