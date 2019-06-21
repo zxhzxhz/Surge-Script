a1bad03ec465b292425d90023dd830cb7eb4386f
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-06-21 21:49:43 +0800

    Update surge bilibili tab.js

--- surge bilibili tab.js
+++ surge bilibili tab.js
@@ -1,14 +1,14 @@
-var obj = JSON.parse(body);
+let body = $response.body
 
-obj['data']['tab'].forEach((element, index) => {
-if(!(["追番","推荐","直播","热门","影视"].includes(element["name"]))) obj['data']['tab'].splice(index,1)  
+body['data']['tab'].forEach((element, index) => {
+if(!(["追番","推荐","直播","热门","影视"].includes(element["name"]))) body['data']['tab'].splice(index,1)  
 });
 
-obj['data']['bottom'].forEach((element, index)=> {
+body['data']['bottom'].forEach((element, index)=> {
     if(element['pos']==4){      
-       obj['data']['bottom'].splice(index,1)  
+       body['data']['bottom'].splice(index,1)  
     }
 });
 
-delete obj['data']['top']
-JSON.stringify(obj); 
+delete body['data']['top']
+$done({body}) 
