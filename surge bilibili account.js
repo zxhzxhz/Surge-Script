b28f618a36a6ad84ac9fdcec42a18bc825be8d2c
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-06-21 21:44:01 +0800

    Update surge bilibili account.js

--- surge bilibili account.js
+++ surge bilibili account.js
@@ -1,6 +1,6 @@
-var obj = JSON.parse(body); 
-obj['data']['sections'].splice(0,1)
-obj['data']['sections'][0]['items'].splice(3,1)
-obj['data']['sections'][0]['items'].splice(4,3)
-obj['data']['sections'].splice(1,1)
-JSON.stringify(obj); 
+let body = $response.body
+body['data']['sections'].splice(0,1)
+body['data']['sections'][0]['items'].splice(3,1)
+body['data']['sections'][0]['items'].splice(4,3)
+body['data']['sections'].splice(1,1)
+$done({body})
